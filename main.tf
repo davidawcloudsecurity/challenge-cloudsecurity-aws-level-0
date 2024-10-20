912c18b774f82f22a51061f8eac8f051379a8909
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-10-20 21:15:59 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,13 +76,6 @@ resource "aws_security_group" "public_security_group" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
