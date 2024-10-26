e9d6a5029356212e481c7fac471871466af5253b
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-10-26 20:13:03 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -184,11 +184,11 @@ resource "aws_instance" "threat_actor" {
   user_data = <<-EOF
 #!/bin/bash
 apt update -y
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-apt install docker-ce
+apt install docker-ce -y
 systemctl status docker
 systemctl start docker
 usermod -aG docker ssm-user
